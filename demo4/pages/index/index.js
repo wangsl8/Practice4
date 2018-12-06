aa519b6a559d1e96a2a811e3d74f4ff285208b1c
WANG <1602840324@qq.com>
2018-12-06 19:47:02 +0800

    Practice
    demo

---
+++ demo4/pages/index/index.js
@@ -0,0 +1,22 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    // text:"这是一个页面"
+    imageSrc: '/image/in1.png',
+    imageSrc1: '/image/in2.png',
+    imageSrc3: '/image/ln31.png',
+    imageSrc4: '/image/ln32.png',
+    imageSrc5: '/image/ln33.png',
+    imageSrc6: '/image/ln34.png',
+    imageSrc7: '/image/ln35.png',
+    imageSrc8: '/image/ln41.png',
+    imageSrc9: '/image/ln41.png',
+
+   
+  },
+  onLoad: function () {
+  }
+})
---
+++ demo4/pages/logs/logs.js
@@ -0,0 +1,31 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    array: [
+
+      {
+
+        url: '/image/logs1.png',
+
+        text: '车用灭火器年检汽车干粉消防器材车载小型便携式灭火器演示培训',
+
+      },
+
+      {
+
+        url: '/image/logs1.png',
+
+        text: '车用灭火器年检汽车干粉消防器材车载小型便携式灭火器演示培训',
+
+
+
+      },
+
+    ],
+
+  },
+  onLoad: function () {
+  }
+})
